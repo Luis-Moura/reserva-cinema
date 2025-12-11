d021b7e4fb3eba871cbd13fccd199de3ec3d643f
Luis-Moura <luish.moura2024@gmail.com>
2025-12-11 09:53:30 -0300

    commit inicial

---
+++ src/main/java/com/reserva_cinema/ReservaCinemaApplication.java
@@ -0,0 +1,13 @@
+package com.reserva_cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaCinemaApplication.class, args);
+	}
+
+}
