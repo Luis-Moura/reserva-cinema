e6e6807bf726f1de852878a4b159989141908f37
Luis-Moura <luish.moura2024@gmail.com>
2025-12-11 19:58:39 -0300

    feat: cria repositório UserRepository para gerenciamento de usuários

---
+++ src/main/java/com/reserva_cinema/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.reserva_cinema.repository;
+
+import com.reserva_cinema.domain.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, String> {
+
+}
