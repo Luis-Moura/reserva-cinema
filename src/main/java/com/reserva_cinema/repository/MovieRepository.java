ebfc01f37a42a96a7f0b3d72c8ddf99460ef743c
Luis-Moura <luish.moura2024@gmail.com>
2025-12-15 11:15:36 -0300

    feat: adiciona suporte à paginação no repositório de filmes

--- src/main/java/com/reserva_cinema/repository/MovieRepository.java
+++ src/main/java/com/reserva_cinema/repository/MovieRepository.java
@@ -1,10 +1,13 @@
 package com.reserva_cinema.repository;
 
 import com.reserva_cinema.domain.entity.MovieEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface MovieRepository extends JpaRepository<MovieEntity, UUID> {
     MovieEntity findByTitle(String title);
+    Page<MovieEntity> findAll(Pageable pageable);
 }
