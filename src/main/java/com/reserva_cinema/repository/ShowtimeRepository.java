e2b1e14c2fc4a409a67d58967e8f8f2323b8698d
Luis-Moura <luish.moura2024@gmail.com>
2025-12-15 23:42:17 -0300

    feat: adiciona método existsByMovieIdAndStartTimeBetween ao ShowtimeRepository

--- src/main/java/com/reserva_cinema/repository/ShowtimeRepository.java
+++ src/main/java/com/reserva_cinema/repository/ShowtimeRepository.java
@@ -3,7 +3,13 @@ package com.reserva_cinema.repository;
 import com.reserva_cinema.domain.entity.ShowtimeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public interface ShowtimeRepository extends JpaRepository<ShowtimeEntity, UUID> {
+    public boolean existsByMovieIdAndStartTimeBetween(
+            UUID movieId,
+            LocalDateTime startTime,
+            LocalDateTime endTime
+    );
 }
