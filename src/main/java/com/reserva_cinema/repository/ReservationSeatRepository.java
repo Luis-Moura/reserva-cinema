3003345140524ad32ceed3241026df1fe3d8e327
Luis-Moura <luish.moura2024@gmail.com>
2025-12-16 19:18:31 -0300

    feat: adiciona repositório ReservationSeatRepository para gerenciamento de assentos de reserva

---
+++ src/main/java/com/reserva_cinema/repository/ReservationSeatRepository.java
@@ -0,0 +1,9 @@
+package com.reserva_cinema.repository;
+
+import com.reserva_cinema.domain.entity.ReservationSeatEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ReservationSeatRepository extends JpaRepository<ReservationSeatEntity, UUID> {
+}
