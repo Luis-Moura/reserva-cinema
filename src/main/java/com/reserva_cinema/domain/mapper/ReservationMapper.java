4187495c66702f5754de5332d726c22c9c70fade
Luis-Moura <luish.moura2024@gmail.com>
2025-12-16 19:08:43 -0300

    feat: adiciona classe ReservationMapper para conversão entre entidades e DTOs de reserva

---
+++ src/main/java/com/reserva_cinema/domain/mapper/ReservationMapper.java
@@ -0,0 +1,34 @@
+package com.reserva_cinema.domain.mapper;
+
+import com.reserva_cinema.api.dto.res.ReservationResponse;
+import com.reserva_cinema.domain.entity.ReservationEntity;
+import com.reserva_cinema.domain.entity.ShowtimeEntity;
+import com.reserva_cinema.domain.entity.UserEntity;
+import com.reserva_cinema.domain.enums.ReservationStatus;
+
+public class ReservationMapper {
+    public ReservationEntity toEntity(
+            ReservationStatus status,
+            Integer totalPrice,
+            ShowtimeEntity showtimeEntity,
+            UserEntity userEntity
+    ) {
+        ReservationEntity entity = new ReservationEntity();
+        entity.setStatus(status);
+        entity.setTotalPrice(totalPrice);
+        entity.setShowtime(showtimeEntity);
+        entity.setUser(userEntity);
+
+        return entity;
+    }
+
+    public ReservationResponse toResponse(ReservationEntity entity) {
+        return new ReservationResponse(
+                entity.getId(),
+                entity.getStatus(),
+                entity.getTotalPrice(),
+                entity.getShowtime().getId(),
+                entity.getUser().getId()
+        );
+    }
+}
