a2f73b3468a282867da54069c2ccbf2820aeaf84
Luis-Moura <luish.moura2024@gmail.com>
2025-12-15 23:49:35 -0300

    feat: adiciona método findAll com paginação ao ShowtimeRepository

--- src/main/java/com/reserva_cinema/repository/ShowtimeRepository.java
+++ src/main/java/com/reserva_cinema/repository/ShowtimeRepository.java
@@ -1,15 +1,19 @@
 package com.reserva_cinema.repository;
 
 import com.reserva_cinema.domain.entity.ShowtimeEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 public interface ShowtimeRepository extends JpaRepository<ShowtimeEntity, UUID> {
-    public boolean existsByMovieIdAndStartTimeBetween(
+    boolean existsByMovieIdAndStartTimeBetween(
             UUID movieId,
             LocalDateTime startTime,
             LocalDateTime endTime
     );
+
+    Page<ShowtimeEntity> findAll(Pageable pageable);
 }
