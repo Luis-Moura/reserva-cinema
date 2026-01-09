59e515e0b2b154d8606eedef76c269d88e8da97e
Luis-Moura <luish.moura2024@gmail.com>
2026-01-09 14:47:03 -0300

    feat: adiciona método para buscar números de assentos por showtime

--- src/main/java/com/reserva_cinema/repository/ReservationSeatRepository.java
+++ src/main/java/com/reserva_cinema/repository/ReservationSeatRepository.java
@@ -2,9 +2,20 @@ package com.reserva_cinema.repository;
 
 import com.reserva_cinema.domain.entity.ReservationSeatEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ReservationSeatRepository extends JpaRepository<ReservationSeatEntity, UUID> {
     long countByShowtimeIdAndSeatNumber(UUID showtimeId, Integer seatNumber);
+
+    @Query(
+    """
+        select rs.seatNumber
+        from ReservationSeatEntity rs
+        where rs.showtime.id = :showtimeId
+    """
+    )
+    List<Integer> findSeatNumbersByShowtimeId(UUID showtimeId);
 }
