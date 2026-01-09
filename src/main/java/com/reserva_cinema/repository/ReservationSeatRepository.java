ceba89e1028fbc29d43ceefd6a878e91f907417d
Luis-Moura <luish.moura2024@gmail.com>
2026-01-09 14:33:49 -0300

    feat: adiciona método para contar assentos por Showtime e número de assento no repositório ReservationSeat

--- src/main/java/com/reserva_cinema/repository/ReservationSeatRepository.java
+++ src/main/java/com/reserva_cinema/repository/ReservationSeatRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface ReservationSeatRepository extends JpaRepository<ReservationSeatEntity, UUID> {
+    long countByShowtimeIdAndSeatNumber(UUID showtimeId, Integer seatNumber);
 }
