52284d1db7186503702a17b04e6f79486966a966
Luis-Moura <luish.moura2024@gmail.com>
2026-01-09 14:33:18 -0300

    feat: adiciona anotações Lombok na classe UserEntity para simplificação de código

--- src/main/java/com/reserva_cinema/domain/entity/UserEntity.java
+++ src/main/java/com/reserva_cinema/domain/entity/UserEntity.java
@@ -3,11 +3,17 @@ package com.reserva_cinema.domain.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "users")
+@Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserEntity {
     @Id
     private String id;
