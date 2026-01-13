f879d0d843c50b6dc6303e5634bff21ac84d9553
Luis-Moura <luish.moura2024@gmail.com>
2026-01-13 16:36:29 -0300

    feat: adiciona método para buscar reservas por ID de usuário com paginação

--- src/main/java/com/reserva_cinema/repository/ReservationRepository.java
+++ src/main/java/com/reserva_cinema/repository/ReservationRepository.java
@@ -1,9 +1,12 @@
 package com.reserva_cinema.repository;
 
 import com.reserva_cinema.domain.entity.ReservationEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface ReservationRepository extends JpaRepository<ReservationEntity, UUID> {
+    Page<ReservationEntity> findAllByUserId(Pageable pageable, String userId);
 }
